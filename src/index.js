996d66a8f59f1476cf97cbefa2b22ee9009db714
Bifeldy <bifeldy@gmail.com>
2023-07-31 18:47:49 +0900

    ✔ Initial Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+export default {
+  async fetch(req, env, ctx) {
+    // console.log('req', req);
+    const { searchParams } = new URL(req.url);
+    if (!searchParams.has('url')) {
+      return Response.redirect('https://www.fansub.id', 301);
+    }
+    let url = searchParams.get('url');
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+      url = 'http://' + url;
+    }
+    if (url.startsWith('http://')) {
+      url = 'https://' + url.slice(7, url.length);
+    }
+    let { readable, writable } = new TransformStream();
+    const fwd = new Request(url, req);
+    const res = await fetch(fwd);
+    res.body.pipeTo(writable);
+    return new Response(readable, res);
+  }
+};
