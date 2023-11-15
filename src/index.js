cfa1c125027b1daac63771028838d467ac2ec30a
Bifeldy <bifeldy@gmail.com>
2023-11-15 22:39:05 +0900

    ✔ Fix :: "Response with null body status (101, 204, 205, or 304) cannot have a body."

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const requestHeadersToRemove = [
 
 export default {
   async fetch(req, env, ctx) {
-    console.log('Request', req);
     const { searchParams } = new URL(req.url);
     if (!searchParams.has('url')) {
       return Response.redirect('https://www.fansub.id', 301);
@@ -18,13 +17,17 @@ export default {
     if (url.startsWith('http://')) {
       url = 'https://' + url.slice(7, url.length);
     }
-    let { readable, writable } = new TransformStream();
     const fwd = new Request(url, req);
     for (const header of requestHeadersToRemove) {
       fwd.headers.delete(header);
     }
     const res = await fetch(fwd);
-    res.body.pipeTo(writable);
-    return new Response(readable, res);
+    if ([101, 204, 205, 304].includes(res.status)) {
+      return new Response(null, res);
+    } else {
+      const { readable, writable } = new TransformStream();
+      res.body.pipeTo(writable);
+      return new Response(readable, res);
+    }
   }
 };
